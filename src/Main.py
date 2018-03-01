489b4c2b82892c43e6f5119cd2da82b988a536f2
jamatofu <clement.beal96@gmail.com>
2018-03-01 18:23:47 +0100

    Edit

--- src/Main.py
+++ src/Main.py
@@ -5,18 +5,20 @@
 import argparse
 
 def writeInFile(exitFile, data):
-    with open(exitFile, 'w') as f:
+    with open('/output/' + exitFile, 'w') as f:
         f.write(data)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "a",
+        type=str,
+    )
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "a",
-    type=str,
-)
 
-# récupère les arguments dans un objet (appelable comme un struct en C)
-args = parser.parse_args()
-print(args.a)
 
-writeInFile('output.txt', args.a)
+    # récupère les arguments dans un objet (appelable comme un struct en C)
+    args = parser.parse_args()
+    print(args.a)
+
+    writeInFile('output.txt', args.a)
