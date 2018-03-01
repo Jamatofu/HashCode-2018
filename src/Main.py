66a3744794a23be7393540ec7dfb8c5dc38a81e5
jamatofu <clement.beal96@gmail.com>
2018-03-01 19:06:16 +0100

    Create basic map

--- src/Main.py
+++ src/Main.py
@@ -16,22 +16,32 @@ def readAndStoreFile(inputFile):
 
     return content
 
+class Map :
+    def __init__(self, row, cols, nbVehicles, nbRides, bonus, nbSteps):
+        self.bonus = bonus
+        self.nbRides = nbRides
+        self.cols = cols
+        self.row = row
+        self.nbVehicles = nbVehicles
+        self.nbSteps = nbSteps
+
+
 
 def startSimulation(file):
-    readAndStoreFile('input/' + file)
+    content = readAndStoreFile(file)
+    print(content)
+    map = Map(content[0][0], content[0][1], content[0][2], content[0][3], content[0][4], content[0][5])
+
 
 
 
 if __name__ == '__main__':
     startSimulation('a_example.in')
-    startSimulation('b_sould_be_easy.in')
-    startSimulation('c_no_hurry.in')
-    startSimulation('d_metropolis.in')
-    startSimulation('e_high_bonus.in')
+    # startSimulation('b_should_be_easy.in')
+    # startSimulation('c_no_hurry.in')
+    # startSimulation('d_metropolis.in')
+    # startSimulation('e_high_bonus.in')
 
 
-
-writeInFile('output.txt', 'a')
 courses = []
-courses = readAndStoreFile('input.txt')
 print(courses)
